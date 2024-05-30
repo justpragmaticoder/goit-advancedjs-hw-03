e2bf7d7334ac75abd8647b9952ed11316012cd94
Vasyl Protsukovych <protcykovich@gmail.com>
2024-05-30 22:33:08 +0200

    Implemented task

---
+++ src/js/main.js
@@ -0,0 +1,75 @@
+import SlimSelect from "slim-select";
+import "slim-select/styles";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { fetchBreeds, fetchCatByBreed } from "./request-helper.js";
+
+function toggleHiddenCompletely(element, isVisible) {
+  element.classList.toggle("is-hidden-compeletely", !isVisible);
+}
+
+function selectorUpload(selector, loader, catDescription) {
+  fetchBreeds()
+    .then(function (breeds) {
+      selector.classList.remove("is-invisible");
+      const slimSelector = breeds.map(
+        ({ id, name }) => `<option value="${id}">${name}</option>`
+      );
+      selector.insertAdjacentHTML("afterbegin", slimSelector.join(""));
+      new SlimSelect({
+        select: selector,
+      });
+    })
+    .catch(function (error) {
+      onError();
+    })
+    .finally(function () {
+      toggleHiddenCompletely(selector, true);
+      toggleHiddenCompletely(loader, false);
+      toggleHiddenCompletely(catDescription, true);
+    });
+}
+
+function createMarkup(event, selector, loader, catDescription) {
+  toggleHiddenCompletely(catDescription, false);
+  toggleHiddenCompletely(loader, true);
+  toggleHiddenCompletely(selector, true);
+
+  const breedId = event.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(function (catData) {
+      if (catData.length === 0) {
+        catDescription.innerHTML = ``;
+      }
+      const { url, breeds } = catData[0];
+
+      catDescription.innerHTML = `
+        <img src="${url}" alt="${breeds[0].name}" width="400" loading="lazy" />
+        <div class="text-box">
+          <h2>${breeds[0].name}</h2>
+          <p>${breeds[0].description}</p>
+          <p><b>Temperament: </b>${breeds[0].temperament}</p>
+          <p><b>Origin: </b>${breeds[0].origin}</p>
+        </div>`;
+    })
+    .catch(function (error) {
+      onError();
+    })
+    .finally(function () {
+      toggleHiddenCompletely(loader, false);
+      toggleHiddenCompletely(selector, true);
+      toggleHiddenCompletely(catDescription, true);
+    });
+}
+
+(() => {
+  const selector = document.querySelector(".breed-select");
+  const loader = document.querySelector(".loader");
+  const catDescription = document.querySelector(".cat-description");
+
+  selector.addEventListener("change", (event) =>
+    createMarkup(event, selector, loader, catDescription)
+  );
+  selectorUpload(selector, loader, catDescription);
+})();
